0f04eb1086f61c9e94d981b538562ec03951e183
Benjamin Hall <b.e.s.hall@outlook.com>
2019-01-22 20:34:20 +0000

    Added BrainTests for TDD of logic

---
+++ FizzBuzzTests/BrainTests.swift
@@ -0,0 +1,21 @@
+//
+//  BrainTests.swift
+//  FizzBuzzTests
+//
+//  Created by beshbashbosh on 22/01/2019.
+//  Copyright © 2019 beshbashbosh. All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzz
+
+class BrainTests: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+
+}
