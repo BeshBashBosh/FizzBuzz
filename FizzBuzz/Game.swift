86e43d258d733d10af7cb3d3979f7db036396737
Benjamin Hall <b.e.s.hall@outlook.com>
2019-01-23 10:38:55 +0000

    Added Test class for Game class

---
+++ FizzBuzz/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  FizzBuzz
+//
+//  Created by beshbashbosh on 23/01/2019.
+//  Copyright © 2019 beshbashbosh. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    
+}
---
+++ FizzBuzzTests/GameTests.swift
@@ -0,0 +1,23 @@
+//
+//  GameTests.swift
+//  FizzBuzzTests
+//
+//  Created by beshbashbosh on 23/01/2019.
+//  Copyright © 2019 beshbashbosh. All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzz
+
+class GameTests: XCTestCase {
+
+    let sut: Game = Game()
+    
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+    
+}
