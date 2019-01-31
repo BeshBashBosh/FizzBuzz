8a18966600d583483fe72ac0b7cc6f2d3102dacb
Benjamin Hall <b.e.s.hall@outlook.com>
2019-01-31 17:46:47 +0000

    Added Move enum that describes possible answers

---
+++ FizzBuzz/Models/Move.swift
@@ -0,0 +1,16 @@
+//
+//  Move.swift
+//  FizzBuzz
+//
+//  Created by beshbashbosh on 31/01/2019.
+//  Copyright © 2019 beshbashbosh. All rights reserved.
+//
+
+import Foundation
+
+enum Move {
+    case number
+    case fizz
+    case buzz
+    case fizzBuzz
+}
