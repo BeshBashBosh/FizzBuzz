e872a26749ba3bbf8b7b3cbeacc9dcf8c5e182fb
Benjamin Hall <b.e.s.hall@outlook.com>
2019-01-31 17:34:52 +0000

    Added accessibility identifier to number button in Main.storyboard. Refenced this in ViewControllerUITests. Added test for number button tapped twice.

--- FizzBuzzUITests/ViewControllerUITests.swift
+++ FizzBuzzUITests/ViewControllerUITests.swift
@@ -10,14 +10,17 @@ import XCTest
 
 class ViewControllerUITests: XCTestCase {
 
+    var app: XCUIApplication!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        app = XCUIApplication()
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -26,12 +29,23 @@ class ViewControllerUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func test_TapNumberButton_IncrementsScore() {
-        XCUIApplication().buttons["0"].tap()
+    func test_TapNumberButton_NumberButtonLabelChangesTo1() {
+        let numberButton = app.buttons["numberButton"]
         
-        let newScore = XCUIApplication().buttons["1"].label
+        numberButton.tap()
+        let newLabel = numberButton.label
         
-        XCTAssertEqual(newScore, "1", "Number button should read '1', reads \(newScore)")
+        XCTAssertEqual(newLabel, "1", "Number button should read '1', reads \(newLabel)")
+    }
+    
+    func test_TapNumberButtonTwice_NumberButtonLabelChangesTo2() {
+        let numberButton = app.buttons["numberButton"]
+        
+        numberButton.tap()
+        numberButton.tap()
+        let newLabel = numberButton.label
+        
+        XCTAssertEqual(newLabel, "2", "Number button should read '2' after being tapped twice from start, reads \(newLabel)")
     }
 
 }
